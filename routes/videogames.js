3796803bf7b3ea9d82b6c2771a3653f026815596
Jacob Roessler <jroessl1@stevens.edu>
2021-12-01 19:53:32 -0500

    Added basic form page for submitting game data

--- routes/videogames.js
+++ routes/videogames.js
@@ -2,6 +2,10 @@ const express = require('express');
 const { videogames } = require('../data');
 const router = express.Router();
 
+router.get('/create', async (req, res) => {
+    res.render('videogames/creategamePage.handlebars', {});
+})
+
 router.get('/:id', async (req, res) => {
     if(!req.params || !req.params.id) {
         res.status(500).render('/error/error', {error: 'No video game id passed in'});
@@ -13,5 +17,37 @@ router.get('/:id', async (req, res) => {
 
     res.render('videogames/videogamesPage.handlebars', {videogameData: videogameData});
 })
+
+router.post('/', async (req, res) => {
+    const { gameTitle, releaseDate, developer, genre, price, boxart } = req.body;
+
+    // Do input validation
+    function stringCheck(str) {
+        return typeof str === 'string' && str.length > 0 && str.replace(/\s/g, "").length > 0;
+    }
+
+    try {
+        if(!gameTitle || !releaseDate || !developer || !genre || !price || !boxart)
+            throw new Error("Missing input");
+        if(!stringCheck(gameTitle) || !stringCheck(releaseDate) || !stringCheck(developer) || !stringCheck(genre) || !stringCheck(price) || !stringCheck(boxart))
+            throw new Error("All strings must contain non-whitespace characters");
+    } catch (e) {
+        res.status(400).render('videogames/creategamePage.handlebars', { error: e.message });
+        return;
+    }
+
+    // Try to add game to database
+    try {
+        const newGame = await videogames.create(gameTitle, releaseDate, developer, genre, price, boxart)
+        if (!newGame) {
+            res.status(400).render('videogames/creategamePage.handlebars', { error: "Game was not successfully added" });
+        } else {
+            res.render('videogames/videogamesPage.handlebars', {videogameData: newGame});
+        }
+    } catch (e) {
+        res.status(400).render('videogames/creategamePage.handlebars', { error: e.message });
+        return;
+    }
+});
 
 module.exports = router;
