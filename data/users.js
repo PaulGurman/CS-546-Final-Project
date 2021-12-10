fb91786b88c0e571d9389e0abc0e5ba1d866bf22
weili <liuweili@lwlmacbook-air.myfiosgat.com>
2021-12-10 01:11:50 -0500

    tiny code cleaning

--- data/users.js
+++ data/users.js
@@ -195,7 +195,7 @@ function checkUserName(str) {
     if (str.length == 0 || str.trim().length == 0) throw "Username cannot be empty!";
     let strArr = str.toLowerCase().split('');
     for (let i = 0; i < strArr.length; i++) {
-        if (strArr[i] === ' ') throw "Username cannot contian space!"
+        if (strArr[i] === ' ') throw "Username cannot contian space!";
     }
     let newStr = strArr.join('');
     for (let i = 0; i < newStr.length; i++) {
@@ -209,7 +209,7 @@ function checkPassword(str) {
     if (str.length == 0 || str.trim().length == 0) throw "Password cannot be empty!";
     let strArr = str.split('');
     for (let i = 0; i < strArr.length; i++) {
-        if (strArr[i] === ' ') throw "Password cannot contian space!"
+        if (strArr[i] === ' ') throw "Password cannot contian space!";
     }
     let newStr = strArr.join('');
     if (newStr.length < 6) throw "Passsword must be at least 6 characters long!";
@@ -219,7 +219,7 @@ function checkLastName(str) {
     if (str.length == 0 || str.trim().length == 0) throw "Last name cannot be empty!";
     let strArr = str.toLowerCase().split('');
     for (let i = 0; i < strArr.length; i++) {
-        if (strArr[i] === ' ') throw "Last name cannot contian space!"
+        if (strArr[i] === ' ') throw "Last name cannot contian space!";
     }
     let newStr = strArr.join('');
     for (let i = 0; i < newStr.length; i++) {
@@ -231,30 +231,26 @@ function checkFirstName(str) {
     if (str.length == 0 || str.trim().length == 0) throw "First name cannot be empty!";
     let strArr = str.toLowerCase().split('');
     for (let i = 0; i < strArr.length; i++) {
-        if (strArr[i] === ' ') throw "First name cannot contian space!"
+        if (strArr[i] === ' ') throw "First name cannot contian space!";
     }
     let newStr = strArr.join('');
     for (let i = 0; i < newStr.length; i++) {
         if (!((newStr.charCodeAt(i) >= 97 && newStr.charCodeAt(i) <= 122))) throw "First name must be characters!";
     }
 }
+
 async function checkUser(username, password) {
     checkUserName(username);
     checkPassword(password);
     lowerUserName = username.toLowerCase();
-    // 1.Query the db for the username supplied, if it is not found, throw an error stating "Either the username or password is invalid".
     const usersCollection = await users();
     const findUserName = await usersCollection.findOne({ username: lowerUserName });
     if (findUserName === null) throw "Either the username or password is invalid";
-
-    // 2. If the username supplied is found in the DB, you will then use bcrypt to compare the hashed password in the database with the password input parameter.
     let compareToMatch = false;
     if (findUserName !== null) {
         hashedPassword = findUserName.password;
         compareToMatch = await bcrypt.compare(password, hashedPassword);
     }
-    // 3. If the passwords match your function will return {authenticated: true}
-    // 4. If the passwords do not match, you will throw an error stating "Either the username or password is invalid"
     if (compareToMatch) {
         return `{authenticated: true}`;
     } else {
@@ -267,7 +263,7 @@ async function getUserId(username) {
     const userCollection = await users();
     const userData = await userCollection.findOne({ username: username });
     userData._id = userData._id.toString();
-    return userData._id
+    return userData._id;
 }
 
 async function checkDuplicateUsername(username){
@@ -275,7 +271,11 @@ async function checkDuplicateUsername(username){
     lowerUserName = username.toLowerCase();
     const usersCollection = await users();
     const findUserName = await usersCollection.findOne({ username: lowerUserName });
-    if (findUserName) throw "Username already exists!";
+    if (findUserName != null){
+        return `{UsernameDuplicate: true}`;
+    }else{
+        return `{UsernameDuplicate: false}`;
+    }
 }
 module.exports = {
     create,
